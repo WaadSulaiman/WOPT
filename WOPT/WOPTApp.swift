27fb5699305b1c70b03f657b2bd91713253e5119
WaadSulaiman <waad.sulaiman@outlook.com>
2021-02-04 14:30:53 +0100

    Git initialized.

---
+++ WOPT/WOPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WOPTApp.swift
+//  WOPT
+//
+//  Created by Waad on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct WOPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
